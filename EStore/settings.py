07c07d4eb9642071d2319dbae8a4a219269081a5
Long Nguyen <nhlong2510@gmail.com>
2023-02-14 16:58:01 +0700

    allowed host
    allowed host

--- EStore/settings.py
+++ EStore/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-u^y@c3g!99kg46ipp4v!(=+5v1&oxlmfotta6k0!z@qxjbmxns
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
